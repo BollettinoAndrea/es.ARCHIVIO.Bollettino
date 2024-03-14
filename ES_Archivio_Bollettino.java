7e8d5016298683586984810f526b5612255a09a6
BollettinoAndrea <160122173+BollettinoAndrea@users.noreply.github.com>
2024-03-14 11:58:24 +0100

    Add files via upload

---
+++ ES_Archivio_Bollettino.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+public class ES_Archivio_Bollettino {
+	public static void main(String[] args) {
+	    
+		Scanner in = new Scanner(System.in);
+		do{
+		    int scelta;
+		    System.out.println("MENU: ");
+		    System.out.println("1 - Aggiunta di un nome");
+		    System.out.println("2 - Cancellazione del singolo nome");
+		    System.out.println("3 - Ricerca sequenziale di un nome");
+		    System.out.println("4 - Visualizza nomi ripetuti con numero ripetizioni");
+		    System.out.println("5 - Modifica di un nome");
+		    System.out.println("6 - Visualizzazione di tutti i nomi presenti");
+		    System.out.println("7 - Ricerca del nome più lungo e più corto");
+		    System.out.println("8 - Cancellazione di tutte le occorrenze di un nome");
+		    System.out.println("0 - Uscita");
+		    System.out.println(" ");
+		    System.out.println("insrisci scelta: ");
+		    
+		
+		    switch(scelta){
+		    
+		    case 1:
+		        
+		    break;
+		    
+		    case 2:
+		        
+		    break;
+		    
+		    case 3:
+		        
+		    break;
+		    
+		    case 4:
+		        
+		    break;
+		    
+		    case 5:
+		        
+		    break;
+		    
+		    case 6:
+		        
+		    break;
+		    
+		    case 7:
+		        
+		    break;
+		    
+		    case 8:
+		        
+		    break;
+		    
+		    case 0:
+		        
+		    break;
+		}
+		}while(scelta =! 0);
+		
+	}
+}
